020293c9e30a2865faf973bc0e855196ea6bba6e
rosghartner <rosghartner@gmail.com>
2021-03-11 19:09:30 +0200

    update 11.03

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('ty_pidor', views.huindex, name="about")
+    path('ty_pidor', views.huindex, name="about"),
+    path('create', views.create, name="create")
     # path('about', views.huindex, name="about")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from main.forms import TaskForm
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from.models import Task
+from.forms import TaskForm
 
 
 def index(request):
@@ -11,4 +13,21 @@ def index(request):
 def huindex(request):
     return render(request,'main/about.html')
    # return HttpResponse("<h1>lol kek cheburek</h1> <hr> ") #HttpResponse это строка через нее неудобно выводить много текста
-    
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'форма была неверной'
+
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request,'main/create.html', context)
